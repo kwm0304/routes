a26bb09f72f9de37fc704467b87096f760a5849d
kwm0304 <106484883+kwm0304@users.noreply.github.com>
2023-07-09 15:17:39 -0400

    added seed data for products

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -1,5 +1,5 @@
 const db = require('./connection')
-const { Location } = require('../models')
+const { Location, Product } = require('../models')
 
 const axios = require('axios')
 const cheerio = require('cheerio')
@@ -45,6 +45,7 @@ axios(url, { headers : {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)
   }).catch(error => {
     console.log('Error:', error)
 })
+//TODO might be faster to just export created json file so it doesn't have to scrape everytime
 //logging to db
 const locations = await Location.insertMany(hospitalInfo)
   .then(() => {
@@ -53,5 +54,93 @@ const locations = await Location.insertMany(hospitalInfo)
   .catch(error => {
     console.log("error", error)
   })
+
+  await Product.deleteMany();
+
+  const products = await Product.insertMany(
+    [
+      {
+        name: "Hospital Bed Sheets",
+        price: 19.99,
+        quantity: 100
+      },
+      {
+        name: "Pillowcases",
+        price: 7.99,
+        quantity: 150
+      },
+      {
+        name: "Patient Gowns",
+        price: 24.99,
+        quantity: 75
+      },
+      {
+        name: "Bath Towels",
+        price: 12.99,
+        quantity: 200
+      },
+      {
+        name: "Hand Towels",
+        price: 6.99,
+        quantity: 250
+      },
+      {
+        name: "Washcloths",
+        price: 4.99,
+        quantity: 300
+      },
+      {
+        name: "Mattress Protectors",
+        price: 29.99,
+        quantity: 50
+      },
+      {
+        name: "Pillow Protectors",
+        price: 9.99,
+        quantity: 100
+      },
+      {
+        name: "Blankets",
+        price: 39.99,
+        quantity: 80
+      },
+      {
+        name: "Bedspreads",
+        price: 49.99,
+        quantity: 60
+      },
+      {
+        name: "Pillows",
+        price: 14.99,
+        quantity: 120
+      },
+      {
+        name: "Draw Sheets",
+        price: 17.99,
+        quantity: 90
+      },
+      {
+        name: "Underpads",
+        price: 11.99,
+        quantity: 100
+      },
+      {
+        name: "Gauze Rolls",
+        price: 8.99,
+        quantity: 200
+      },
+      {
+        name: "Surgical Drapes",
+        price: 29.99,
+        quantity: 80
+      },
+      {
+        name: "Pillow Covers",
+        price: 6.99,
+        quantity: 150
+      }
+    ])
+    console.log('Products seeded')
+
   process.exit()
 })
